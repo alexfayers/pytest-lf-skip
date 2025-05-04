6e2e8de4d0a2835c5fa000522cb58799b1127546
alexfayers <alex@fayers.com>
2025-05-04 01:39:19 +0100

    test: remove unnecessary print statement and assertion in version test

--- tests/test_lf_skip.py
+++ tests/test_lf_skip.py
@@ -216,7 +216,3 @@ def test_package_version() -> None:
             "0.1.0",
         ]
     ), f"Version is {__version__} which indicates that git tags have not been cloned"
-
-    print(__version__)
-
-    assert False
